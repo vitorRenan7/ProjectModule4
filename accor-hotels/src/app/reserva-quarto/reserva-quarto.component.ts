bdd56b285a09543b6f3ad3491ab80e3240c6e105
Vitor Renan <113404754+Vitao7@users.noreply.github.com>
2023-03-07 12:55:24 -0300

    Telas

---
+++ accor-hotels/src/app/reserva-quarto/reserva-quarto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reserva-quarto',
+  templateUrl: './reserva-quarto.component.html',
+  styleUrls: ['./reserva-quarto.component.scss']
+})
+export class ReservaQuartoComponent {
+
+}
