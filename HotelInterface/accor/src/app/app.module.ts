5c10d33d2654a2cfe61a71dfcff4179a6f29da58
Vitor Renan <113404754+Vitao7@users.noreply.github.com>
2023-03-08 14:17:24 -0300

    fix: Criação do componente pra tela de Reserva de Quarto

--- HotelInterface/accor/src/app/app.module.ts
+++ HotelInterface/accor/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ReservaQuartoComponent } from './reserva-quarto/reserva-quarto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ReservaQuartoComponent
   ],
   imports: [
     BrowserModule,
