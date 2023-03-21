bf1152d395dad5db3d3700ed16a4d449d6f6a60a
Vitor Renan <113404754+Vitao7@users.noreply.github.com>
2023-03-21 16:36:31 -0300

    feat: Adicionando Rotas da pagina de 'Reserva de Quarto

--- Hotel-Interface/accor/src/app/app-routing.module.ts
+++ Hotel-Interface/accor/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { LoginComponent } from './login/login.component';
+import { ReservaQuartoComponent } from './reserva-quarto/reserva-quarto.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'reserva-quarto', component: ReservaQuartoComponent },
+  { path: '', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
