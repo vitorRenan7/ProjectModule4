e84b528f9d0a0f6e601786bad6e58b950f690063
Vitor <vitaocodes@gmail.com>
2023-04-03 23:51:55 -0300

    refactor: Exclusão de caracter no 'Login.component' para resolução de bug

--- Hotel-Interface/accor/src/app/login/login.component.ts
+++ Hotel-Interface/accor/src/app/login/login.component.ts
@@ -5,4 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {gi}
+export class LoginComponent {}
